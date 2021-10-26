afa850881e558ce21191c0130df345699c52c99f
gendeathz <lehiennhan2000@gmail.com>
2021-10-26 15:13:33 +0700

    Đang lỗi
    Hôm qua chạy dc 2 - 3 cái epoch ra được loss giờ nó lỗi cmn luôn @@

--- main.py
+++ main.py
@@ -29,18 +29,39 @@ trainY = np.array(labelTrain)
 testX = np.array(labelTest)
 testY = np.array(vectorTest)
 
+print('Prepare data completed')
 
 # 2. Build model 
 model = Sequential()
 model.add(Dense(1, input_shape=((None, 2560))))
-model.add(Activation('sigmoid'))
+# model.add(Dense(128))
+# model.add(Dense(1, input_shape=((None, 2560))))
+# model.add(Activation('linear'))
+# model.add(Activation(activation = "softmax"))
+# model.add(Dense(64))
+# model.add(Dense(nb_classes,name="outputlayer"))
+# model.add(Dense(32)) 
+# model.add(Activation('softmax'))
+# model.add(Dense(16,activation='softmax'))
+# model.add(Dense(8))
+# model.add(Flatten())
+# model.add(Dense(4))
+# model.add(Dense(nb_classes, activation='softmax'))
+# model.add(Dense(units=128, input_shape=(None,2560), activation='softmax'))
+# model.add(Dense(units=128, activation='softmax'))
+# model.add(Dropout(0.25))
+# model.add(Dense(units=6, activation='softmax'))
+
+print('Created an model!')
 
 # 3. gradient descent optimizer and loss function 
 sgd = optimizers.SGD(lr=0.05)
-model.compile(loss=losses.binary_crossentropy, optimizer=sgd)
-
+#model.compile(loss=losses.binary_crossentropy, optimizer=sgd)
+model.compile(loss='categorical_crossentropy',
+                optimizer='adam',
+                metrics=['accuracy'])
 # 4. Train the model 
-model.fit(trainX, trainY, epochs=20, batch_size=1) 
+model.fit(trainX, trainY,validation_data=(testX,testY) ,epochs=20, batch_size=1, verbose=2) 
 print("Train complete");
 #
 #Test the model
