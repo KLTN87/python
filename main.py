aab70537b6471ba20c474f04c47073e46aefe3e3
Moleys <34809211+Moleys@users.noreply.github.com>
2021-10-27 17:02:00 +0700

    thêm convert sang .pb

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ from tensorflow.keras.utils import to_categorical
 
 
 # 1.
-path0 = '1-training-dave.txt'
-path1 = '1-testing-dave.txt'
+path0 = '1-testing-dave.txt'
+path1 = '1-training-dave.txt'
 
 labelTrain = txt2Arr(path0,"label")
 vectorTrain = txt2Arr(path0,"vector")
@@ -96,16 +96,62 @@ print("Testing on test data")
 # Print the model's accuracy
 print("Accuracy="+ str(accuracy))
 
-# print(trainX)
 
 # print("One hot")
 # print(trainY_one_hot)
-
 # print("Normal")
-
 # print(trainY)
 
-predict_x = model.predict(testX)[96] 
+
+# Test predict 
+from random import randrange
+lenTextX = len(testX)
+rd = randrange(lenTextX)
+predict_x = model.predict(testX)[rd] 
 classes_x = np.argmax(predict_x, axis=-1)
+print("Test predict")
 print(classes_x)
-print(testY[96])
+print(testY[rd])
+
+
+
+# convert to .pb
+
+from tensorflow.python.framework.convert_to_constants import convert_variables_to_constants_v2
+
+# Convert Keras model to ConcreteFunction
+full_model = tf.function(lambda x: model(x))
+full_model = full_model.get_concrete_function(
+    x=tf.TensorSpec(model.inputs[0].shape, model.inputs[0].dtype))
+
+# Get frozen ConcreteFunction
+frozen_func = convert_variables_to_constants_v2(full_model)
+frozen_func.graph.as_graph_def()
+
+# inspect the layers operations inside your frozen graph definition and see the name of its input and output tensors
+layers = [op.name for op in frozen_func.graph.get_operations()]
+print("-" * 50)
+print("Frozen model layers: ")
+for layer in layers:
+    print(layer)
+
+print("-" * 50)
+print("Frozen model inputs: ")
+print(frozen_func.inputs)
+print("Frozen model outputs: ")
+print(frozen_func.outputs)
+
+# Save frozen graph from frozen ConcreteFunction to hard drive
+# serialize the frozen graph and its text representation to disk.
+tf.io.write_graph(graph_or_graph_def=frozen_func.graph,
+                  logdir="./frozen_models",
+                  name="simple_frozen_graph.pb",
+                  as_text=False)
+
+#Optional
+tf.io.write_graph(graph_or_graph_def=frozen_func.graph,
+                  logdir="./frozen_models",
+                  name="simple_frozen_graph.pbtxt",
+                as_text=True)
+
+model.summary()
